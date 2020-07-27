c5cd9eefaa4b079e26112a8c071e1d7ca62a36e8
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-07-27 03:00:25 -0400

    Add files via upload

---
+++ python_176.py
@@ -0,0 +1,22 @@
+class auto:
+  def __init__(engine, motor, wheels):
+    transport.engine = engine
+    transport.motor = motor
+    transport.wheels = wheels
+
+
+class foreign(auto):
+  def __init__ (compact, aesthetic appeal, fuel efficient):
+    transport.compact = compact
+    transport.aesthetic appeal = appeal
+    transport.fuel efficient = fuele
+
+
+class domestic(auto):
+  def __init__(larger, fuel inefficient, economical ):
+    transport.larger = larger
+    transport.fuel inefficient = fueli
+    transport.economical = economical
+
+
+

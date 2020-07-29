7ea77120196ff7bd8a32ddad1af1fa05ed57d649
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-07-29 15:48:56 -0400

    Add files via upload

--- python_176.py
+++ python_176.py
@@ -1,22 +1,21 @@
 class auto:
-  def __init__(self,engine,motor,wheels):
-    self.engine = engine
-    self.motor = motor
-    self.wheels = wheels
-
+    engine = ' '
+    "all"-wheel drive = True
+    motor = ' ' 
+    wheels = True
+    power steering = True
 
 class foreign(auto):
-  def __init__ (compact, aesthetic appeal, fuel efficient):
-    self.compact = compact
-    self.aesthetic appeal = appeal
-    self.fuel efficient = fuele
+    compact = True
+    aesthetic appeal = True
+    fuel = ' '
+
 
 
 class domestic(auto):
-  def __init__(larger, fuel inefficient, economical ):
-    self.larger = larger
-    self.fuel inefficient = fueli
-    self.economical = economical
+    larger = True
+    fuel  = ' '
+    economical = True
 
 
 

4da8faded73153aa3459f54ef98efb942675d210
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-07-31 08:36:30 -0400

    Add files via upload

---
+++ learning_tinkter.py
@@ -0,0 +1,37 @@
+import tkinter
+from tkinter import*
+#can change resize window to True to give user ability to resize window
+#can change background color by adding self.master.config(bg='#000')
+
+class ParentWindow(Frame):
+    def __init__ (self,master):
+        Frame.__init__(self)
+
+        self.master = master
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(700, 400))
+        self.master.title('Learning Tkinter!')
+        self.master.config(bg='lightgray')
+
+        self.varFName = StringVar()
+        self.varLName = StringVar()
+        self.varFName.set('Bob')
+        self.varFName.set('Smith')
+
+        print(self.varFname.get())
+        print(self.varLname.get())
+
+        self.txtFName = Entry(self.master,text=self.varFName, font=("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtFName.pack()
+
+        self.txtLName = Entry(self.master,text=self.varLName, font=("Helvetica", 16), fg='black', bg='lightblue')
+        self.txtLName.pack()
+        
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
+                             

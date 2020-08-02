ea1a7c23dc9b45014523c3046664c0b78c960497
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-08-01 21:09:40 -0400

    Add files via upload

--- python_200.py
+++ python_200.py
@@ -1,11 +1,12 @@
 class soccer_player:
-    def __init__(self, name, score):
+    def __init__(self):
         self._name= "Dave"  # protected attribute 
         self.__score= 5 # private attribute
 
 
-        soccer_player_a = sp() #protected object
-        print(sp_a.name)
+object = soccer_player() #protected object
+print(object._name)
 
-        soccer_player_b = sp() #private object
-        print(sp_b.score)
+object = soccer_player() #protected object
+print(object.__score)
+

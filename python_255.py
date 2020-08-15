d6a3ab59cac88d06054a5e7ab7256376b4678391
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-08-15 19:15:40 -0400

    Add files via upload

--- python_255.py
+++ python_255.py
@@ -16,7 +16,7 @@ master.minsize(400,150)
 
 originPath = '/Users/Mely Mel/Desktop/A'
 
-
+originPath = '/Users/Mely Mel/Desktop/B'
 
 
 
@@ -25,10 +25,20 @@ originPath = '/Users/Mely Mel/Desktop/A'
 def opendirectory():
     rep = filedialog.askdirectory(parent=master, initialdir=originPath)
     if rep:
-        print(rep)
+        e1.delete(0)
+        e1.insert(0,rep)
+
     else:
         print('Cancelled') 
 
+def opendirectory2():
+   rep = filedialog.askdirectory(parent=master, initialdir=originPath)
+   if rep:
+        e2.delete(0)
+        e2.insert(0,rep)
+
+   else:
+        print('Cancelled') 
 
  
 
@@ -44,27 +54,19 @@ e2.grid(row=1, column=1, columnspan=1, ipadx=55)
 
 
 
-
-
-
 #create first button
 button1 = tk.Button(master, text="Browse...", command = opendirectory)
 button1.grid(row=0, column=0, columnspan=1, ipadx=19,pady=10, padx=20)
 
-
-
-
-
-
  
  
-# Create another button
-button2 = tk.Button(master, text="Browse...")
+# Create 2nd button
+button2 = tk.Button(master, text="Browse...",command = opendirectory2)
 button2.grid(row=1, column=0, columnspan=1, ipadx=19, pady=10, padx=20)
 
 
 
-# Create another button
+# Create 3rd button
 button3 = tk.Button(master, text="Check for files...")
 button3.grid(row=2, column=0, columnspan=1, ipadx=2, pady=10, padx=5)
 

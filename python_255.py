3d70346abe686287ab992ff242e476280b18aa62
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-08-26 19:57:06 -0400

    Add files via upload

--- python_255.py
+++ python_255.py
@@ -19,16 +19,6 @@ master.minsize(400,150)
 e1 = tk.Entry(master)
 e2 = tk.Entry(master)
 
- ##shows that you're getting the path successfully
-src = e1.get()
-print(src)  
-##shows that you're getting the path successfully 
-dst = e2.get()
-print(dst)
-
-files = os.listdir(src)
-print(files)
-
 
 
 def SourceBrowse():
@@ -52,18 +42,23 @@ def DestinationBrowse():
 
 
 def DestinationCheck():
-        for i in files: ##starts loop with your file list
-            now=datetime.now() ##current time
-            timeModified = datetime.fromtimestamp(os.path.getmtime(src +'/'+ i))##finds the time this current individual file was last modified. The src+file gets the absolute path
-            timeDiff = (now - timeModified) ##finds the difference between now and when the file was modified
+    src = e1.get()
+    print(src)
+    dst = e2.get()
+    print(dst)
+    files = os.listdir(src)
+    for i in files: ##starts loop with your file list
+        now=datetime.now() ##current time
+        timeModified = datetime.fromtimestamp(os.path.getmtime(src +'/'+ i))##finds the time this current individual file was last modified. The src+file gets the absolute path
+        timeDiff = (now - timeModified) ##finds the difference between now and when the file was modified
         print (timeDiff)
-        if timeDiff > timedelta(days=1): ##See if the difference is larger than one day, if it is, it means the file hasn't been created or modified in the last day
-            print ("Status: Old")
-            print (i)
-        else: ##if its not larger than one day it means its a newly modified or created file
-            print ("Status: New")
-            print (i)
-            shutil.move(src+'/'+i,dst)  ##if you don't use the .join method you need to insert your own slash or it can't find the file
+    if timeDiff > timedelta(days=1): ##See if the difference is larger than one day, if it is, it means the file hasn't been created or modified in the last day
+        print ("Status: Old")
+        print (i)
+    else: ##if its not larger than one day it means its a newly modified or created file
+        print ("Status: New")
+        print (i)
+        shutil.move(src+'/'+i,dst)  ##if you don't use the .join method you need to insert your own slash or it can't find the file
 
 
 

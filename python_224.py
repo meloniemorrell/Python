5e0859ccda450b7a1f3035fbea71b34038a22d79
meloniemorrell <64949163+meloniemorrell@users.noreply.github.com>
2020-09-18 12:39:40 -0700

    Add files via upload

---
+++ python_224.py
@@ -0,0 +1,16 @@
+from ABC import ABC, abstractmethod
+class smartphone(ABC):
+    def communicate(self,text)
+        print('Message: ', text)
+    @abstractmethod
+    def touchscreen(self, text):
+        pass
+
+class tablet(self,text):
+    def touchscreen(self,text)
+    
+        print('Message: ', text)
+
+obj = tablet()
+obj.communicate()
+obj.touchscreen()

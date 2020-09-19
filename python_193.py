ff3d0f653860a04a346114ab4eded306405a4e64
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-18 20:42:13 -0700

    Update python_193.py

--- python_193.py
+++ python_193.py
@@ -20,29 +20,30 @@ class MultiFamily(Residence):
     gated = True
 
     def getresidentType(self):
-        entry_structure = input("Enter material: ")
-        entry_flooring = input("Enter flooring type: ")
-        entry_floors = input("Enter number of floors: ")
-        entry_private = input("Enter True or False: ")
-        if entry_private == "True":
-            print('true')    
-        elif entry_private == "False":
-            print('false')
+    entry_structure = input("Enter material: ")
+    entry_flooring = input("Enter flooring type: ")
+    entry_floors = input("Enter number of floors: ")
+    entry_gated = input("Enter True or False: ")
+    if entry_gated == "True":
+        print('true')    
+    elif entry_gated == "False":
+        print('false')
+
 
 
 class SingleFamily(Residence):
     number_of_homes = 100
     parking = 'garage'
-    gated = True
+    security = True
 
     def getresidentType(self):
         entry_structure = input("Enter material: ")
         entry_flooring = input("Enter flooring type: ")
         entry_floors = input("Enter number of floors: ")
-        entry_private = input("Enter True or False: ")
-        if entry_private == "True":
+        entry_security = input("Enter True or False: ")
+        if entry_security == "True":
             print('true')    
-        elif entry_private == "False":
+        elif entry_security == "False":
             print('false')
 
 structure = Residence()

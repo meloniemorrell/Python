55743c98bc637145ba0f9d725a9df1115fdc4e64
meloniemorrell <meloniemorrell73@gmail.com>
2020-09-19 14:17:49 -0700

    updates

--- python_171.py
+++ python_171.py
@@ -4,38 +4,37 @@ conn = sqlite3.connect('test.db')
 
 
 with conn:
-	cur = conn.cursor()
-	cur.execute("CREATE TABLE IF NOT EXISTS tbl_techacad( \
-	ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-	col_fileList STRING \
-	)")
-	conn.commit()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_techacad( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fileList STRING \
+        )")
+    conn.commit()
 conn.close()
 
 conn = sqlite3.connect('test.db')
 
 with conn:
-	cur = conn.cursor()
-	cur.execute("INSERT INTO tbl_techacad (fileList) VALUES \
-				('information.docx'), \
-				('Hell.txt'), \
-				('myImage.png'), \
-				('myMovie.mpg'), \
-				('World.txt'), \
-				('data.pdf'), \
-				('myPhoto.jpg')"
-				)
-	conn.commit()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_techacad (col_fileList) VALUES \
+                ('information.docx'), \
+                ('Hell.txt'), \
+                ('myImage.png'), \
+                ('myMovie.mpg'), \
+                ('World.txt'), \
+                ('data.pdf'), \
+                ('myPhoto.jpg')"
+                )
+    conn.commit()
 conn.close()
 
 conn = sqlite3.connect('test.db')
 
 with conn:
-	cur = conn.cursor()
-	cur.execute("SELECT FROM tbl_techacad WHERE col_fileList LIKE '%xt'")
-	varFile = cur.fetchall()
-	for item in varFile:
-            msg = 'Files name: {}'.format(item)
-            print(msg)	
-                                    
-				
+    cur = conn.cursor()
+    cur.execute("SELECT col_fileList FROM tbl_techacad WHERE col_fileList LIKE '%xt'")
+    varFile = cur.fetchall()
+    for item in varFile:
+        msg = 'File name: {}'.format(item)
+        print(msg)
+    
